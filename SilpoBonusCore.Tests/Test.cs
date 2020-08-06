1f278f5fd2924ef1d8d79c1b962484e3126145f1
Oleksandr Bukovskyi <oleksandrbu@tahometer.com>
2020-08-06 15:07:52 +0300

    Test for check auto open

--- SilpoBonusCore.Tests/Test.cs
+++ SilpoBonusCore.Tests/Test.cs
@@ -28,6 +28,19 @@ namespace SilpoBonusCore.Tests
             Assert.Equal(10, check.getTotalCost());
         }
 
+        [Fact]
+        void addProduct__whenCheckIsClosed__opensNewCheck() {
+            CheckoutService checkoutService = new CheckoutService();
+            checkoutService.openCheck();
+            
+            checkoutService.addProduct(new Product(7, "Milk"));
+            Check milkCheck = checkoutService.closeCheck();
+            Assert.Equal(7, milkCheck.getTotalCost());
+
+            checkoutService.addProduct(new Product(3, "Bred"));
+            Check bredCheck = checkoutService.closeCheck();
+            Assert.Equal(3, bredCheck.getTotalCost());
+        }
 
     }
 }
